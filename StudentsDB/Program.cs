173e8a75ed02e044457a5011d9120611019a29a0
max Icolumi <max77648@gmail.com>
2021-05-06 10:37:29 +0300

    Добавил меню

--- StudentsDB/Program.cs
+++ StudentsDB/Program.cs
@@ -12,29 +12,90 @@ namespace StudentsDB
     {
         static void Main(string[] args)
         {
-            XmlDocument xmlDocument = new XmlDocument();
-            try
-            {
-                xmlDocument.Load("DB/Students.xml");
-            }
-            catch (Exception exc)
-            {
-                Console.WriteLine(exc.Message);
-                Console.ReadKey();
-                Environment.Exit(1);
-            }
-           
+            //XmlDocument xmlDocument = new XmlDocument();
+            //try
+            //{
+            //    xmlDocument.Load("DB/Students.xml");
+            //}
+            //catch (Exception exc)
+            //{
+            //    Console.WriteLine(exc.Message);
+            //    Console.ReadKey();
+            //    Environment.Exit(1);
+            //}
 
-            XmlNode xRoot = xmlDocument.DocumentElement;
-            foreach (XmlNode node in xRoot)
-            {
-                foreach (XmlNode childNode in node.ChildNodes)
-                {
-                    Console.WriteLine(childNode.InnerText);
-                }
-            }
 
+            //XmlNode xRoot = xmlDocument.DocumentElement;
+            //foreach (XmlNode node in xRoot)
+            //{
+            //    foreach (XmlNode childNode in node.ChildNodes)
+            //    {
+            //        Console.WriteLine(childNode.InnerText);
+            //    }
+            //}
+            ChooseCommand();
             Console.ReadKey();
+        }
+
+        static void ChooseCommand()
+        {
+            bool isExit = false;
+            bool isCorrect = false;
+            int numberOfCommand = 0;
+            string input;
+            while (isExit == false)
+            {
+                Console.WriteLine("Выберете команду: ");
+                Console.WriteLine("\t 1 - показать студентов.");
+                Console.WriteLine("\t 2 - показать предметы.");
+                Console.WriteLine("\t 3 - добавить предмет.");
+                Console.WriteLine("\t 4 - добавить студента.");
+                Console.WriteLine("\t 5 - показать предметы конкретного студента.");
+                Console.WriteLine("\t 6 - показать студентов конкретного предмета.");
+                Console.WriteLine("\t 7 - выход.");
+                Console.Write("\t 8 - очистить консоль.\n>");
+                
+                while (isCorrect == false)
+                {
+                    input = Console.ReadLine();
+                    if (int.TryParse(input, out numberOfCommand))
+                    {
+                        isCorrect = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Введите корректный номер команды!");
+                    }
+                }
+
+                switch (numberOfCommand)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7: 
+                        isExit = true;
+                        Console.WriteLine("Программа закончила свою работу!");
+                        break;
+                    case 8:
+                        Console.Clear();
+                        break;
+
+                    default:
+                        Console.WriteLine("Неверная команда!");
+                        break;
+                }
+                isCorrect = false;
+            }
         }
     }
 
